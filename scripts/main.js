aeef83e7244d19009ac7adc329a9d684f93ef88e
Sherzod38 <nadirovsherzod82@gmail.com>
2021-01-06 15:38:42 +0500

    uyga vazifa

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,46 @@
-console. log('Hello world!');
+var btnToast = document.querySelector('.btn-toast')
+var toast = document.querySelector('.toast')
+btnToast.addEventListener('click', function() {
+  toast.classList.add('show')
+})
+
+var btnCloze = document.querySelector('.closes')
+btnCloze.addEventListener('click', function(){
+  toast.classList.remove('show')
+})
+// console. log('Hello world!');
+
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
+var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+  container: 'body'
+})
+
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl, option)
+})
+
+var myToastEl = document.getElementById('myToast')
+myToastEl.addEventListener('shown.bs.toast', function () {
+  // do something...
+  myToastEl.toast('show');
+})
